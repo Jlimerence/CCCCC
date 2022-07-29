21f973fa79c66168f7ad8ef9407f447060b1997c
Jlimerence <cuoguodeshiyihan@gmail.com>
2022-07-29 18:14:18 +0800

    每日练习

--- Project3/源.c
+++ Project3/源.c
@@ -247,30 +247,245 @@
 //}
 
 //数组指针的使用
-void print1(int(*pa)[5] ,int x,int y)
+//void print1(int(*pa)[5] ,int x,int y)
+//{
+//	int i = 0, j = 0;
+//	for (i = 0; i < x; i++)
+//	{
+//		for (j = 0; j < y; j++)
+//		{
+//			printf("%d ",*(*(pa + i) + j));//pa是 指向一行的地址，加上个i跳过i行，*（pa+i）相当于找到了这行，拿到了这行的数组名，*（pa+i）+j相当于找到了这一行第j个元素
+//			//printf("%d ", (*(pa + i))[j]);
+//			//printf("%d ",pa[i][j]);
+//		}
+//		printf("\n");
+//	}
+//
+//}
+//int main()
+//{	
+//	int arr[3][5] = { {1,2,3,4,5},{2,3,4,5,6},{3,4,5,6,7} };
+//
+//	print1(&arr, 3, 5);
+//
+//	return 0;
+//}
+//去掉数组名和后面的方块就是数组里面元素的类型
+
+
+
+//函数指针:指向函数的指针(存放的是函数的地址)Add表示函数的地址和&Add一样
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//
+//}
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	int ret = Add(a, b);
+//	int(*p)(int,int)=Add;//p后面的括号表明这是一个函数指针
+//	printf("%d\n", (*p)(3, 7));
+//	printf("%d\n",(p)(3,7));//写p也可以  *p也可以
+//	printf("%d\n", ret);
+//
+//	return 0;
+//}
+
+//void Print(char* str)//用一个一级指针来接收，可以传递的是：1、一级指针；2、某个变量的地址（数组首元素的地址）
+//{
+//	printf("%s\n",str);
+//}
+//int main()
+//{
+//	void(*p)(char*) = Print;//去掉p就是函数的类型
+//	(*p)("fire");
+//
+//	return 0;
+//}
+
+//
+//void (*  signal(int, void(*)(int) )  )(int);//去掉函数名和函数的参数就是函数的返回类型
+////简化
+//typedef void(*pfun_t)(int);
+//pfun_t(signal(int, pfun_t));
+////signal是一个函数声明
+////signal函数的参数有两个，第一个是int，第二个是函数指针，该函数指针指向函数的参数是int，返回类型是void。
+////signal函数的返回类型也是函数指针，该函数指针指向函数的参数是int，返回类型是void。
+
+
+//函数指针数组（数组去掉数组名和【】就是数组里面元素的类型）
+
+
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//int Sub(int x, int y)
+//{
+//	return x - y;
+//}
+//int Mul(int x, int y)
+//{
+//	return x * y;
+//}
+//int Div(int x, int y)
+//{
+//	return x / y;
+//}
+//int main()
+//{
+//	//int(*p)(int, int) = Add;
+//	int(*p[4])(int, int) = { Add,Sub,Mul,Div };
+//	int i = 0;
+//	for (i = 0; i < 4; i++)
+//	{
+//		printf("%d\n", p[i](2, 3));	//p[i]--->访问下标为i的数组元素，就是访问函数的地址，可以不进行解引用
+//	}
+//	return 0;
+//}
+//整型数组：数组元素类型是整型
+//函数指针数组：数组元素类型是函数指针类型
+
+
+//最朴实无华的加减乘除
+
+int Add(int x, int y)
 {
-	int i = 0, j = 0;
-	for (i = 0; i < x; i++)
-	{
-		for (j = 0; j < y; j++)
-		{
-			printf("%d ",*(*(pa + i) + j));//pa是 指向一行的地址，加上个i跳过i行，*（pa+i）相当于找到了这行，拿到了这行的数组名，*（pa+i）+j相当于找到了这一行第j个元素
-			//printf("%d ", (*(pa + i))[j]);
-			//printf("%d ",pa[i][j]);
-		}
-		printf("\n");
-	}
+	return x + y;
+}
+int Sub(int x, int y)
+{
+	return x - y;
+}
+int Mul(int x, int y)
+{
+	return x * y;
+}
+int Div(int x, int y)
+{
+	return x / y;
+}
+int Xor(int x, int y)
+{
+	return x ^ y;
+}
+void menu()
+{
+	printf("*****************************\n");
+	printf("****1.Add          2.Sub*****\n");
+	printf("****3.Mul          4.Div*****\n");
+	printf("*******   0.exit    *********\n");
+}
+//int main()
+//{
+//	int input = 0;
+//	int a = 0, b = 0;
+//	do
+//	{
+//		menu();
+//		printf("请选择：\n");
+//		scanf("%d", &input);
+//		if (input != 0)
+//		{
+//			printf("请输入操作数：\n");
+//			scanf("%d%d", &a, &b);
+//		}
+//		switch (input)
+//		{
+//		case 1:printf("%d\n",Add(a, b));
+//			break;
+//		case 2:printf("%d\n",Sub(a, b));
+//			break;
+//		case 3:printf("%d\n",Mul(a, b));
+//			break;
+//		case 4:printf("%d\n",Div(a, b));
+//			break;
+//		case 0:printf("退出\n");
+//			break;
+//		default:printf("选择错误\n");
+//			break;
+//		}
+//	} while (input);
+//
+//	return 0;
+//}
+
+//使用函数指针数组的高效方法实现简单运算------>转移表
+//int main()
+//{
+//	int input = 0;
+//	int a = 0, b = 0;
+//	do
+//	{
+//		menu();
+//		printf("请选择：\n");
+//		scanf("%d", &input);
+//		//pfArr是一个数组，是一个函数指针数组，数组里面存放的是函数的地址
+//		int(*pfArr[])(int, int) = { 0,Add,Sub,Mul,Div ,Xor};//初始化第一个元素为0
+//		if (input >= 1 && input <= 5)
+//		{
+//			printf("请输入两个操作数：\n");
+//			scanf("%d%d", &a, &b);
+//			int ret = pfArr[input](a, b);
+//			printf("%d\n", ret);
+//		}
+//		else if (input == 0)
+//		{
+//			printf("退出\n");
+//		}
+//		else
+//		{
+//			printf("输入错误\n");
+//		}
+//
+//	} while (input);
+//}
+
+//通过回调函数改进switch代码						回调函数--------->用2函数来接收1函数的地址，在2函数内部实现1函数的功能
+
+void Calc(int(*pfArr)(int,int))
+{
+	int x = 0, y = 0;
+	printf("请输如两个操作数：\n");
+	scanf("%d%d", &x, &y);
+	int ret = pfArr(x, y);
+	printf("%d\n", ret);
 
 }
-int main()
-{	
-	int arr[3][5] = { {1,2,3,4,5},{2,3,4,5,6},{3,4,5,6,7} };
 
-	print1(&arr, 3, 5);
+
+int main()
+{
+	int input = 0;
+	do
+	{
+		menu();
+		printf("请选择：\n");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:Calc(Add);
+			break;
+		case 2:Calc(Sub);
+			break;
+		case 3:Calc(Mul);
+			break;
+		case 4:Calc(Div);
+			break;
+		case 0:printf("退出\n");
+			break;
+		default:printf("选择错误\n");
+			break;
+		}
+	} while (input);
 
 	return 0;
 }
-//去掉数组名和后面的方块就是数组里面元素的类型
+
 
 
 
