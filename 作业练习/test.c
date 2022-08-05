052d811b82012217eabcd27bd7eddf7860b9d786
Jlimerence <cuoguodeshiyihan@gmail.com>
2022-08-05 09:57:45 +0800

    练习

--- 作业练习/test.c
+++ 作业练习/test.c
@@ -324,32 +324,123 @@ void move(char* arr, int k)
 
 //写一个函数判断B字符串是不是A经过翻转得到的
 //方法一：穷举法
-#include<string.h>
-int Is_leftmove(char* arr1, char* arr2)
-{	
-	assert(arr1 != 0 && arr2 != 0);
-	int len = strlen(arr1);
-	int i = 0;
-	for (i = 0; i < len; i++)
-	{
-		move(arr1, 1);
-		if (strcmp(arr1, arr2) == 0)
-			return 1;
-	}
-	return 0;
-}
-int main()
-{
-	char arr1[] = "abcdef";
-	char arr2[] = "cdefab";
-	int ret= Is_leftmove(arr1, arr2);
-	if (ret == 1)
-		printf("Yes\n");
-	else
-		printf("NO\n");
-	return 0;
-}
+//#include<string.h>
+//int Is_leftmove(char* arr1, char* arr2)
+//{	
+//	assert(arr1 != 0 && arr2 != 0);
+//	int len = strlen(arr1);
+//	int i = 0;
+//	for (i = 0; i < len; i++)
+//	{
+//		move(arr1, 1);
+//		if (strcmp(arr1, arr2) == 0)
+//			return 1;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	char arr1[] = "abcdef";
+//	char arr2[] = "cdefab";
+//	int ret= Is_leftmove(arr1, arr2);
+//	if (ret == 1)
+//		printf("Yes\n");
+//	else
+//		printf("NO\n");
+//	return 0;
+//}
 
 //方法二
 //abcdefabcdef
 //字符串B如果是A经过旋转得到的，那一定是A+A的子集
+
+
+//strcat函数不能自己追加自己，会引发崩溃
+//\0 位置在追加的时候被改成第一个字符，导致追加后的字符串\0的位置变得随机了
+//#include<string.h>
+//int main()
+//{
+//	char arr1[30] = {"abcdef"};
+//	char arr2[] = "hh";
+//	strcat(arr1, arr2);
+//	printf("%s",arr1);
+//	return 0;
+//}
+
+
+//#include<string.h>
+//int Is_leftmove(char* str1, char* str2)
+//{	
+//	int len1 = strlen(str2);
+//	int len2 = strlen(str1);
+//	if (len1 != len2)//如果两个字符串长度不一致，那么一定不是旋转得来的，但是有可能是字串。所以需要提前判断一下
+//		return 0;
+//	//1、在str1字符串后追加一个str1字符串
+//	strncat(str1, str1, len1);
+//	//2、判断str2是不是1的子集
+//	//strstr--找字串的
+//	char* ret=strstr(str1, str2);//str2是否是str1的子串，如果是返回str2指向的地址，否则返回空指针
+//	if (ret == NULL)
+//	{
+//		//找不到
+//		return 0;
+//	}
+//	else
+//	{
+//		return 1;
+//	}
+//}
+//
+//
+//int main()
+//{
+//	char arr1[30] = "abcdef";
+//	char arr2[] = "cdefab";
+//	int ret = Is_leftmove(arr1, arr2);
+//	if (ret == 1)
+//		printf("Yes\n");
+//	else
+//		printf("NO\n");
+//	return 0;
+//}
+
+//杨氏矩阵
+//有一个数字矩阵，矩阵的每行从左到右是递增的，矩阵从上到下是递增的，请编写程序在这样的矩阵中查找某个数字是否存在。
+//要求时间复杂度小于O(N)---->不能使用穷举法一个个的找
+//1 2 3
+//4 5 6
+//7 8 9
+
+//解题思路：利用左下或者右上这两个比较特殊的点
+
+//int Findnum(int arr[3][3], int k, int row, int col)
+//{
+//	int x = 0;
+//	int y = col - 1;
+//	while (y>=0&&x<=row-1)
+//	{
+//		if (arr[x][y] > k)
+//		{
+//			y--;
+//		}
+//		else if (arr[x][y] < k)
+//		{
+//			x++;
+//		}
+//		else
+//			return 1;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[3][3] = { 1,2,3,4,5,6,7,8,9 };
+//	int k = 7;
+//	int ret = Findnum(arr, k, 3, 3);//这里也可以定义x、y然后传地址过去，在FInd函数内部就可以直接改变main函数内部的x、y的值
+//	if (ret == 1)
+//		printf("找到了\n");
+//	else
+//		printf("找不到\n");
+//
+//	return 0;
+//}
