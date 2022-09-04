940763a718646e44840b43228d3ef57104d66313
Jlimerence <cuoguodeshiyihan@gmail.com>
2022-09-04 22:42:56 +0800

    练习

---
+++ Project6/test.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdlib.h>
+#include<stdio.h>
+//int main()
+//{
+//	while (1)
+//	{
+//		malloc(1);
+//	}
+//
+//	return 0;
+//}
+
+int main()
+{
+	int a = 1;
+	printf("%f", (double)a);
+
+	return 0;
+}
